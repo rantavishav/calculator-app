153cf4af8fcb1d7020415c0ea22bc1eac50053ed
rantavishav49m <rantavishav49@gmail.com>
2021-07-13 16:38:29 +0530

    Optimized Theme Code

--- main.js
+++ main.js
@@ -108,35 +108,72 @@ const themeButton = (event) =>{
 
 // Function Which Will Change Theme w.r.t the input (1,2,3)
 const changeTheme = (themeNumber) => {
-    if(themeNumber === 1){
-        swapCssFile("./theme/theme-1.css");
+    var rootNode= document.querySelector(':root');
 
+    if(themeNumber === 1){
+        // Chnage Toogle_circle Visbility
+        document.getElementById("theme-circle-1").style.visibility = "visible";
+        document.getElementById("theme-circle-2").style.visibility = "hidden";
+        document.getElementById("theme-circle-3").style.visibility = "hidden";
+        // Change Background Varibale Values
+        rootNode.style.setProperty("--main-bg-color", `hsl(222, 26%, 31%)`); 
+        rootNode.style.setProperty("--display-screen-bg", `hsl(224, 36%, 15%)`);  
+        rootNode.style.setProperty("--keypad-bg", `hsl(223, 31%, 20%)`) ;
+
+        // Change Key Variables Values
+        rootNode.style.setProperty("--del-reset-bg", `hsl(225, 21%, 49%)`);
+        rootNode.style.setProperty("--del-reset-box-shadow", `hsl(224, 28%, 35%)`);
+        rootNode.style.setProperty("--toggle-color", `hsl(6, 63%, 50%)`);
+        rootNode.style.setProperty("--toggle-box-shadow", `hsl(6, 70%, 34%)`);
+        rootNode.style.setProperty("--buttons-bg", `hsl(30, 25%, 89%)`) ;
+        rootNode.style.setProperty("--buttons-box-shadow", `hsl(28, 16%, 65%)`) ;
+
+        // Chnage Text Variable Value
+        rootNode.style.setProperty("--main-text-color",  `hsl(221, 14%, 31%)`);
+        rootNode.style.setProperty("--main-title-color",  `hsl(0, 0%, 100%)`);
     }else if(themeNumber === 2){
-        swapCssFile("./theme/theme-2.css");
+        // Chnage Toogle_circle Visbility
+        document.getElementById("theme-circle-1").style.visibility = "hidden";
+        document.getElementById("theme-circle-2").style.visibility = "visible";
+        document.getElementById("theme-circle-3").style.visibility = "hidden";
+
+         // Change Background Varibale Values
+         rootNode.style.setProperty("--main-bg-color", `hsl(0, 0%, 90%)`);
+         rootNode.style.setProperty("--display-screen-bg", `hsl(0, 0%, 93%)`);
+         rootNode.style.setProperty("--keypad-bg", `hsl(0, 5%, 81%)`);
+ 
+         // Change Key Variables Values
+         rootNode.style.setProperty("--del-reset-bg", `hsl(185, 42%, 37%)`);
+         rootNode.style.setProperty("--del-reset-box-shadow", `hsl(185, 58%, 25%)`);
+         rootNode.style.setProperty("--toggle-color", `hsl(25, 98%, 40%)`);
+         rootNode.style.setProperty("--toggle-box-shadow", `hsl(25, 99%, 27%)`);
+         rootNode.style.setProperty("--buttons-bg", `hsl(45, 7%, 89%)`);
+         rootNode.style.setProperty("--buttons-box-shadow", `hsl(35, 11%, 61%)`);
+ 
+         // Chnage Text Variable Value
+         rootNode.style.setProperty("--main-text-color", `hsl(60, 10%, 19%)`);
+         rootNode.style.setProperty("--main-title-color",  `hsl(60, 10%, 19%)`);
     }else if(themeNumber === 3){
-        swapCssFile("./theme/theme-3.css")
+        // Chnage Toogle_circle Visbility
+        document.getElementById("theme-circle-1").style.visibility = "hidden";
+        document.getElementById("theme-circle-2").style.visibility = "hidden";
+        document.getElementById("theme-circle-3").style.visibility = "visible";
+
+        // Change Background Varibale Values
+        rootNode.style.setProperty("--main-bg-color", `hsl(268, 75%, 9%)`);
+        rootNode.style.setProperty("--display-screen-bg", `hsl(268, 71%, 12%)`);
+        rootNode.style.setProperty("--keypad-bg", `hsl(268, 71%, 12%)`);
+
+        // Change Key Variables Values
+        rootNode.style.setProperty("--del-reset-bg", `hsl(281, 89%, 26%)`);
+        rootNode.style.setProperty("--del-reset-box-shadow", `hsl(285, 91%, 52%)`);
+        rootNode.style.setProperty("--toggle-color", `hsl(176, 100%, 44%)`);
+        rootNode.style.setProperty("--toggle-box-shadow", `hsl(176, 100%, 44%)`);
+        rootNode.style.setProperty("--buttons-bg", `hsl(268, 47%, 21%)`);
+        rootNode.style.setProperty("--buttons-box-shadow", `hsl(290, 70%, 36%)`);
+
+        // Chnage Text Variable Value
+        rootNode.style.setProperty("--main-text-color", `hsl(52, 100%, 62%)`);
+        rootNode.style.setProperty("--main-title-color",  `hsl(52, 100%, 62%)`);
     }
 }
-
-// Helper Function For Chanding Theme
-
-const swapCssFile = (path) =>{
-     //remove previous stylesheet
-     document.querySelectorAll('[rel="stylesheet"]')[0].remove();
-        
-     // Add new Style sheet
-     // Get HTML head element
-     var head = document.getElementsByTagName('HEAD')[0]; 
-
-     // Create new link Element
-     var linkNode = document.createElement('link');
-
-     // set the attributes for link element 
-     linkNode.rel = 'stylesheet';  
-     linkNode.type = 'text/css';
-     linkNode.href = path; 
-
-     // Append linkNode element to HTML head
-     return head.appendChild(linkNode);
-}
-
